88cd83ef1bde034c2f96350d7dfd0947502aa0c3
Aaron Hessler <aaron_hessler@outlook.de>
2023-11-17 15:28:06 +0100

    Added header.

---
+++ components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="header w-screen">
+        <nuxt-link to="/">
+            <img src="@/assets/svgs/logo.svg" alt="">
+        </nuxt-link>
+        <nuxt-link to="/login">Login</nuxt-link>
+        <nuxt-link to="/learn">Learn</nuxt-link>
+        <nuxt-link to="/test">Test</nuxt-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .header {
+        gap: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 20px;
+        padding: 20px 30px;
+
+        img {
+            width: 100px;
+            margin-right: 10px;
+        }
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <template>
     <div>
-        <Placeholder />
     </div>
 </template>
