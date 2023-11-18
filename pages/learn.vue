d4c4c64bbf75571e73bea29bb99d0adeac135a91
AaronHessler <aaron_hessler@outlook.de>
2023-11-18 17:19:11 +0100

    Added option to select vocab.

--- pages/learn.vue
+++ pages/learn.vue
@@ -1,10 +1,22 @@
+
+import type { Btn } from '#build/components';
+
 <template>
     <OptionsPanel title="Learn" icon="heroicons-solid:academic-cap">
-        <Btn>Vocab</Btn>
+        <Btn id="vocab"><Icon name="heroicons-solid:dots-horizontal" />Select Vocab</Btn>
+        <Btn>Vocabulary</Btn>
         <Btn>Translation</Btn>
         <Btn>Declination</Btn>
         <Btn>Conjugation</Btn>
     </OptionsPanel>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    #vocab {
+        margin-bottom: 15px;
+
+        svg {
+            margin-right: 8px;
+        }
+    }
+</style>
