f0d0f794e1774e452979e042ccaf9587506cc30f
Aaron Hessler <aaron_hessler@outlook.de>
2023-11-20 13:21:39 +0100

    Made lesson pages dynamic.

--- pages/learn/index.vue
+++ pages/learn/index.vue
@@ -1,12 +1,17 @@
 <template>
     <OptionsPanel title="Learn" icon="heroicons-solid:academic-cap">
         <Btn id="selection"><Icon name="heroicons-solid:dots-horizontal"/>Select lesson</Btn>
-        <Btn href="/learn/vocab"><Icon name="heroicons-solid:collection"/> Vocabulary</Btn>
+        <Btn :href=lessonUrl><Icon name="heroicons-solid:collection"/> Vocabulary</Btn>
         <Btn><Icon name="heroicons-solid:translate"/> Translation</Btn>
         <Btn><Icon name="heroicons-solid:beaker"/>Declination</Btn>
         <Btn><Icon name="heroicons-solid:book-open"/>Conjugation</Btn>
     </OptionsPanel>
 </template>
+
+<script setup>
+    let lesson = await useCookie("lesson");
+    let lessonUrl = "/learn/vocab/" + lesson.value;
+</script>
 
 <style lang="scss" scoped>
     #selection {
---
+++ pages/learn/vocab/[lesson].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <OptionsPanel :title=lesson></OptionsPanel>
+    </div>
+</template>
+
+<script setup>
+    const { lesson } = useRoute().params;
+</script>
+
+<style lang="scss" scoped>
+
+</style>
